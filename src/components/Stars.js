a97f2dbead1b29e043e274df33c4f53407f8b803
Dana Ken <dana@tealbook.com>
2024-03-13 09:50:35 -0400

    fixed radius error, removed three from package.

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -5,7 +5,7 @@ import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
   const ref = useRef();
-  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
+  const sphere = random.inSphere(new Float32Array(6000), { radius: 1.4 });
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
@@ -17,7 +17,7 @@ const Stars = (props) => {
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
-          color="#f272c8"
+          color="#2cbaf5"
           size={0.002}
           sizeAttenuation={true}
           depthWrite={false}
